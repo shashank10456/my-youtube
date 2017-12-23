1cc1dc1429849b6f65a867230e7b57a7269f9b54
shashank10456 <shashank10456@gmail.com>
2017-12-23 11:56:35 +0530

    first commit

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,23 +1,6 @@
-/**
- * Created by lokeshagrawal on 07/05/17.
- */
 import React from 'react';
 
-/*
-const VideoListItem = (props) => {
-    const video = props.video;
-    return(
-        <li>
-            Video
-        </li>
-    );
-}
-*/
-
-// ES 6 way, it says an object has a property called video please grab that and declare a new variable called video.
 const VideoListItem = ({video, onVideoSelect}) => {
-    // const video = props.video;
-    // const onVideoSelect = props.onVideoSelect;
 
     return(
         <li onClick={()=>onVideoSelect(video)} className="list-group-item">
